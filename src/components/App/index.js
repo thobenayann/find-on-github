cd8266d04709ac823140f8615f47a13bc40dc7be
thobenayann <63640040+thobenayann@users.noreply.github.com>
2021-03-01 15:51:23 +0100

    :lipstick: feat(Loader):set-loader

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 import SearchBar from '../SearchBar';
 import Message from '../Message';
 import ReposResults from '../ReposResults';
+import Loader from '../Loader';
 
 // == Import static data
 import data from '../../data/repos';
@@ -17,11 +18,16 @@ function App() {
   // tableau de nos repos dans le state
   const [repos, setRepos] = useState([]);
   // Nombre de résultats de recherche
-  const [totalCount, setTotalCount] = useState(null);
   // null est falsy
+  const [totalCount, setTotalCount] = useState(null);
+  // booléen indiquant le chargement de data
+  const [isLoading, setIsLoading] = useState(false);
+
 
   // requête axios pour la récupération de la liste des repos
   const onSubmit = (searchTerm) => {
+    // On active le Loader
+    setIsLoading(true);
     // Query string avec notre variable searchTerm récupérée depuis l'input de SemanticUI
     axios.get(`https://api.github.com/search/repositories?q=${searchTerm}`)
       .then((response) => {
@@ -30,7 +36,12 @@ function App() {
       })
       .catch((error) => {
         console.log('error', error);
-      });
+      })
+      .finally(() => {
+        // ce que contient finally sera exécuté à la fin de la requête
+        // qu'elle échoue ou non
+        setIsLoading(false);
+      })
   };
 
   return (
@@ -40,7 +51,11 @@ function App() {
       />
       {/* affichage conditionnel, si on a pas de repos, on affiche pas le message */}
       { totalCount && <Message nbRepos={totalCount} />}
-      <ReposResults repoList={repos} />
+      {/* Utilisation d'un ternaire pour conditionner l'affichage de notre Loader */}
+      { isLoading
+          ? <Loader />
+          : <ReposResults repoList={repos} />
+      }
     </div>
   );
 }
---
+++ src/components/Loader/index.js
@@ -0,0 +1,11 @@
+// == Import npm
+import React from 'react';
+
+// == Import components
+import {Loader as SemanticLoader} from 'semantic-ui-react';
+
+const Loader = () => (
+    <SemanticLoader active size='massive'/>
+);
+
+export default Loader;
